ded13a4dacc960192813380d786ea00b2c32e2b7
Courtney Hollenkamp <cmhollenkamp@gmail.com>
2018-12-21 14:03:19 -0600

    edit and showactions complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   def index
     @users = User.all 
   end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
   
   def new
     @user = User.new
@@ -12,9 +16,23 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to @user
     else
       render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
     end
   end
 
